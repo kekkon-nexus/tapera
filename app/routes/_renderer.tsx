70dba3906aec194a46f919b9154b111774913fad
Yuuki Rika <yvvki@duck.com>
2024-06-06 21:38:57 +0700

    Add favicons

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -6,7 +6,13 @@ export default jsxRenderer(({ children, title }, c) => (
 		<head>
 			<meta charset="UTF-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta name="theme-color" content="#0d6" />
 			{title ? <title>{title}</title> : <></>}
+
+			<link rel="icon" href="/static/favicon.svg" />
+			<link rel="mask-icon" href="/static/favicon.svg" color="#0d6" />
+			<link rel="apple-touch-icon" href="/static/apple-touch-icon.png" />
+			<link rel="manifest" href="/static/app.webmanifest" />
 
 			<Link rel="stylesheet" href="/style.css" />
 			<Script src="/app/client.ts" async nonce={c.get("secureHeadersNonce")} />
