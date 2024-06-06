42a58462a6bc540d3dff2afb313242deaaa5cfda
Yuuki Rika <yvvki@duck.com>
2024-06-06 14:20:31 +0700

    Setup `tailwindcss`

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,4 +1,5 @@
 import { jsxRenderer } from "hono/jsx-renderer";
+import { Link } from 'honox/server'
 
 export default jsxRenderer(({ children, title }) => (
 	<html lang="en">
@@ -6,6 +7,8 @@ export default jsxRenderer(({ children, title }) => (
 			<meta charset="UTF-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			{title ? <title>{title}</title> : <></>}
+
+			<Link rel="stylesheet" href="/style.css" />
 		</head>
 		<body>{children}</body>
 	</html>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,7 @@
 import { createRoute } from "honox/factory";
 
 export default createRoute((c) =>
-	c.render(<h1>Tapera tapi jelek!</h1>, { title: "Tapera" }),
+	c.render(<h1 class="text-4xl font-bold">Tapera tapi jelek!</h1>, {
+		title: "Tapera",
+	}),
 );
