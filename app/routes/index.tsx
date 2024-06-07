ff5b9c6d5af37dea10548001464e8b11a780630a
Yuuki Rika <yvvki@duck.com>
2024-06-07 17:46:17 +0700

    Improve landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,21 +2,27 @@ import { createRoute } from "honox/factory";
 
 export default createRoute((c) =>
 	c.render(
-		<div class="hero min-h-screen bg-base-200">
-			<div class="hero-content text-center">
-				<div class="max-w-md">
-					<h1 class="text-5xl font-bagnard">Hello there</h1>
-					<p class="py-6">
-						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-						excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-						a id nisi.
-					</p>
-					<button type="button" class="btn btn-primary">
-						Get Started
-					</button>
+		<>
+			<div id="hero" class="hero min-h-screen bg-base-200">
+				<div class="hero-content text-center">
+					<div class="max-w-md">
+						<h1 class="font-bagnard">
+							UUD 1945
+							<br />
+							Pasal 28H Ayat 1
+						</h1>
+						<p class="py-4">
+							Setiap orang berhak hidup sejahtera lahir dan batin, bertempat
+							tinggal, dan mendapatkan lingkungan hidup yang baik dan sehat
+							serta berhak memperoleh pelayanan kesehatan.
+						</p>
+						<button type="button" class="m-4 btn btn-primary">
+							Informasi Lebih
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>,
+		</>,
 		{
 			title: "Tepar",
 		},
