b4d498afa0ea6278f908f1a2ad6374458bc0f743
Yuuki Rika <yvvki@duck.com>
2024-06-08 01:15:05 +0700

    Fix translation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -39,7 +39,7 @@ const Hero = () => (
 
 const Services = () => (
 	<div id="services" class="m-4">
-		<h2 class="px-10 pt-10 text-4xl font-bagnard text-center">Servis</h2>
+		<h2 class="px-10 pt-10 text-4xl font-bagnard text-center">Jasa</h2>
 		<div class="flex flex-row flex-wrap">
 			<ServiceCard
 				href="#"
@@ -93,7 +93,7 @@ const ServiceCard = ({ href, icon, title, description }: ServiceCardProp) => (
 
 const Supporters = () => (
 	<div id="supporters">
-		<h2 class="px-10 pt-10 text-4xl font-bagnard text-center">Suporter</h2>
+		<h2 class="px-10 pt-10 text-4xl font-bagnard text-center">Pendukung</h2>
 		<div class="divider pt-4">Inspirasi</div>
 		<SupporterList
 			size="h-[36rem]"
